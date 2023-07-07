4bc0732e6c7659e0d706097a3bdbf797e01f54cf
Anmol Kaushik <anmol18129@iiitd.ac.in>
2023-07-07 20:36:18 +0530

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/',(req,res)=>{
-    res.send("This is my Nodejs app");
+    res.send("Hurray! We have deployed our app");
 })
 
 app.listen(port,()=>{
